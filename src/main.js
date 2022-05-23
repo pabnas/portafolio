5cdf68462ee31e90805dc9bc6fc9669028528d67
santiago b <sbarraganp19@gmail.com>
2022-05-23 00:56:11 -0500

    add popperjs

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 createApp(App).mount('#app')
