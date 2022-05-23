9bc72104af2ee66af03b1de2fe62e84e584b5fa3
santiago b <sbarraganp19@gmail.com>
2022-05-23 01:17:10 -0500

    added NavBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <nav-bar/>
   <home-page/>
 </template>
 
 <script>
 import HomePage from "@/components/Home";
+import NavBar from "@/components/NavBar";
 
 export default {
   name: 'App',
   components: {
+    NavBar,
     HomePage,
   }
 }
